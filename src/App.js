a72a9efeb5c4c40d18360bdd479839b27b6c7908
Mark Wild <mark.wild189@gmail.com>
2020-03-14 14:07:04 -0700

    commit

--- src/App.js
+++ src/App.js
@@ -3,8 +3,8 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import { Header } from './components/Header';
 import { Quote } from './components/Quote';
 import { Content } from './components/Content';
-import { Photos } from './components/pages/Photos';
-import { Testimonials } from './components/pages/Testimonials';
+import Photos from './components/pages/Photos';
+import Testimonials from './components/pages/Testimonials';
 
 import './App.css';
 
--- src/components/pages/Photos.js
+++ src/components/pages/Photos.js
@@ -1,7 +1,11 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 
 export const Photos = () => {
-  return <div>photo page</div>;
+  return (
+    <Fragment>
+      <h1>photo page</h1>
+    </Fragment>
+  );
 };
 
 export default Photos;
