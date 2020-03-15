11f37249b40930cb9a823db8dbfe51012d334eb1
Mark Wild <mark.wild189@gmail.com>
2020-03-15 16:36:21 -0700

    Updates

---
+++ precache-manifest.c394063e60ec2b2421505e631542dada.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a27bf80f25f25bbee8f768f7b94b749a",
+    "url": "/save-team/index.html"
+  },
+  {
+    "revision": "5504c3a1072836286668",
+    "url": "/save-team/static/css/main.2c3d3e00.chunk.css"
+  },
+  {
+    "revision": "c69061162be59eeec00c",
+    "url": "/save-team/static/js/2.5f53fb48.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/save-team/static/js/2.5f53fb48.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5504c3a1072836286668",
+    "url": "/save-team/static/js/main.a969d6e6.chunk.js"
+  },
+  {
+    "revision": "7ac06c55794ded085fcc",
+    "url": "/save-team/static/js/runtime-main.318c3bd7.js"
+  },
+  {
+    "revision": "0a89595596b9271ec3fcfe13bc0c1dc1",
+    "url": "/save-team/static/media/DSC_4693.0a895955.jpg"
+  },
+  {
+    "revision": "ed6170bccef080840c30ac17e6d950af",
+    "url": "/save-team/static/media/diogo-tavares-rcngqAAfYK0-unsplash.ed6170bc.jpg"
+  }
+]);
