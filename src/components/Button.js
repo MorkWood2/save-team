718f8b61ba3feb42ebac103ea512efc1a787cd87
Mark Wild <mark.wild189@gmail.com>
2020-03-15 16:23:38 -0700

    commit

--- src/components/Button.js
+++ src/components/Button.js
@@ -14,13 +14,7 @@ export const Button = () => {
         >
           Sign the Petition
         </a>
-        <div className='footer'>
-          Made with{' '}
-          <span role='img' aria-label='heart'>
-            ❤️
-          </span>{' '}
-          by the Team class of 06-07
-        </div>
+        <div className='footer'>Made by the Team class of 06-07</div>
       </div>
     </div>
   );
