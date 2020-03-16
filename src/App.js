dc0e27dd0642959e438f8ae2a85d0d1d4fdd7b36
Mark Wild <mark.wild189@gmail.com>
2020-03-15 18:01:43 -0700

    remove router

--- src/App.js
+++ src/App.js
@@ -1,22 +1,13 @@
 import React from 'react';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import { Home } from './components/pages/Home';
-import Photos from './components/pages/Photos';
-import Testimonials from './components/pages/Testimonials';
 
 import './App.css';
 
 function App() {
   return (
-    <Router>
-      <div className='App'>
-        <Switch>
-          <Route exact path='/' component={Home} />
-          <Route exact path='/testimonials' component={Testimonials} />
-          <Route exact path='/photos' component={Photos} />
-        </Switch>
-      </div>
-    </Router>
+    <div className='App'>
+      <Home />
+    </div>
   );
 }
 
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,21 +1,9 @@
 import React from 'react';
-// import { Link } from 'react-router-dom';
 
 export const Header = () => {
   return (
     <div>
       <div className='header-background'>
-        {/* <nav>
-          <ul>
-            <li>
-              <Link to='/photos'>Photos</Link>
-            </li>
-            <li>
-              <Link to='/testimonials'>Testimonials</Link>
-            </li>
-          </ul>
-        </nav> */}
-
         <p className='header'>Save the Team Program!!!</p>
         <a href='#end' className='header-button'>
           Save Team!
