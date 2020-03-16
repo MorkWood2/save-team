7a3f61f7c86f2efd1e974617a7c9ad177d10b986
Mark Wild <mark.wild189@gmail.com>
2020-03-15 17:52:49 -0700

    Updates

---
+++ precache-manifest.95094ad1804cf0aeee26acc6602682b5.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c5f5d5ee8dceb5387357ca4237f923bf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0c6025a969640d5c06b5",
+    "url": "/static/css/main.2f44ee91.chunk.css"
+  },
+  {
+    "revision": "c69061162be59eeec00c",
+    "url": "/static/js/2.5f53fb48.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/static/js/2.5f53fb48.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0c6025a969640d5c06b5",
+    "url": "/static/js/main.a969d6e6.chunk.js"
+  },
+  {
+    "revision": "eb8970bc8403aa34a466",
+    "url": "/static/js/runtime-main.150b02bd.js"
+  },
+  {
+    "revision": "0a89595596b9271ec3fcfe13bc0c1dc1",
+    "url": "/static/media/DSC_4693.0a895955.jpg"
+  },
+  {
+    "revision": "ed6170bccef080840c30ac17e6d950af",
+    "url": "/static/media/diogo-tavares-rcngqAAfYK0-unsplash.ed6170bc.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/save-team/precache-manifest.ccf6de534f8395bc39bffe1811b0ea22.js"
+  "/precache-manifest.95094ad1804cf0aeee26acc6602682b5.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/save-team/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
